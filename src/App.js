407b88356fd0a015dc973b79cb9b7288ddf5d084
notry345 <notry345@naver.com>
2024-06-05 15:44:56 +0900

    hi

---
+++ src/App.js
@@ -0,0 +1,48 @@
+/* eslint-disable */
+// import React, { useStaet } from 'react'
+import logo from './logo.svg';
+import './App.css'; // 여기서 스타일링
+import { useState } from 'react';
+
+function App() { // JS 함수 문법
+
+
+  let [title, title변경] = useState(['오늘의 기분', '우동 맛집', '저녁 메뉴 추천']); // ES6 destructing 문법
+  let [like, like변경] = useState(0);
+
+  let posts = 'Thoughts' // 데이터 바인딩을 위해 변수에 데이터 저장
+
+  function title바꾸기(){
+    var newTitle = [...title]; // deep copy
+    newTitle[0] = '내일의 기분'
+    title변경(newTitle);
+  }
+
+
+  return (
+  // JSX : 리액트에서 사용하는 HTML 대용 문법
+    <div className="App">
+      <div className="black-nav">
+        <div style={{color: 'pink', fontSize: '30px'}}>내 Blog</div>
+      </div>
+      <button onClick={ title바꾸기 }>버튼</button>
+      <div className="list">
+        <h3> { title[0] } <span onClick={ ()=>{ like변경(like+1) } }>👍🏻</span> { like } </h3>
+        <p>6월 5일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { title[1] } </h3>
+        <p>6월 5일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { title[2] } </h3>
+        <p>6월 5일 발행</p>
+        <hr/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// 우리는 App.js에 코드를 작성하지만
+// 실제로 렌더링되는건 html 파일
+// public/index.html의 id가 root인 div 태그에 App.js를 다 갖다줌
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
